cdae08d43872e3ece1b76ed8dde9e9611e199ecf
suredo <scampossilva@gmail.com>
2021-05-14 00:31:59 -0300

    docs(server): comments/doc added to server class

--- server/server.ts
+++ server/server.ts
@@ -2,6 +2,10 @@ import express from "express";
 
 export class Server {
   private app: express.Application;
+
+  /**Server configuration
+   * @param port - Port number on which the server will listen on
+   */
   constructor(private port: number) {
     this.app = express();
   }
