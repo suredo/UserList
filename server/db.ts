c64731178307542e875c4564384dd0a013f264fc
suredo <scampossilva@gmail.com>
2021-05-14 02:48:47 -0300

    docs(db): description correction

--- server/db.ts
+++ server/db.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const db = "mongodb://localhost:27017/dev";
 
-/**Connects server mongoDb */
+/**Connects server to mongoDb */
 export const connect = async () => {
   return await mongoose.connect(db, {
     useNewUrlParser: true,
